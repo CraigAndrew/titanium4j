e038ac3bdb1546b110febab19cbf35e10f294c26
eemi2010 <jazzmatadazz@gmail.com>
2013-04-07 18:49:32 +0200

    implementing ios calendar module

---
+++ src/com/emitrom/ti4j/mobile/client/ios/calendar/RecurrenceRule.java
@@ -0,0 +1,78 @@
+/**************************************************************************
+ * RecurrenceRule.java is part of Titanium4j Mobile 3.0. Copyright 2012 Emitrom
+ * LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ **************************************************************************/
+
+package com.emitrom.ti4j.mobile.client.ios.calendar;
+
+import java.util.List;
+
+import com.emitrom.ti4j.mobile.client.core.events.EventDispatcher;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArray;
+
+/**
+ * 
+ * An object that represents a single reminder for an event in an Android
+ * calendar.
+ * <p>
+ * Reminders should be created using the
+ * Titanium.Android.Calendar.Event.createReminder method rather than directly.
+ * <p>
+ * See Titanium.Android.Calendar for examples of retrieving reminder information
+ * and creating reminders for events.
+ * 
+ */
+public class RecurrenceRule extends EventDispatcher {
+
+    protected RecurrenceRule(JavaScriptObject obj) {
+        jsObj = obj;
+    }
+
+    /**
+     * @return The id of the reminder.
+     */
+    public native String getId() /*-{
+		var jso = this.@com.emitrom.ti4j.core.client.ProxyObject::getJsObj()();
+		return jso.id;
+    }-*/;
+
+    /**
+     * @return The reminder method. possible values are the method constants in
+     *         {@linkcom.ekambi.gwt.appcelerator.mobile.client.android.calendar.Calendar}
+     * 
+     */
+    public native int getMethod() /*-{
+		var jso = this.@com.emitrom.ti4j.core.client.ProxyObject::getJsObj()();
+		return jso.method;
+    }-*/;
+
+    /**
+     * @return The minutes before the event when the reminder should occur.
+     */
+    public native int getMinutes() /*-{
+		var jso = this.@com.emitrom.ti4j.core.client.ProxyObject::getJsObj()();
+		return jso.minutes;
+    }-*/;
+
+    static JavaScriptObject fromList(List<RecurrenceRule> values) {
+        JsArray<JavaScriptObject> peers = JsArray.createArray().cast();
+        for (RecurrenceRule rule : values) {
+            peers.push(rule.getJsObj());
+        }
+        return peers;
+    }
+
+}
